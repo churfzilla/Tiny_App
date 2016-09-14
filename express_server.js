b828f5465f178946cb5ebad368da4c96fd234c2b
Chad Bruhaug <churfzilla@gmail.com>
2016-09-14 16:02:20 -0700

    displays urls in list through passing data

--- express_server.js
+++ express_server.js
@@ -1,24 +1,32 @@
 var express = require("express");
 var app = express();
+app.set("view engine", "ejs");
 var PORT = process.env.PORT || 8080; // default port 8080
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
+app.get('/urls', (req, res) => {
+    var urlDatabase = [
+        { shortUrl: "b2xVn2", fullUrl: "http://www.lighthouselabs.ca" },
+        { shortUrl: "9sm5xK", fullUrl: "http://www.google.com" }
+    ];
+    var tagline = "Displaying my links.";
+
+    res.render('urls_index', {
+        urlDatabase: urlDatabase,
+        tagline: tagline
+    });
+});
+
+// var urlDatabase = {
+// "b2xVn2": "http://www.lighthouselabs.ca",
+// "9sm5xK": "http://www.google.com"
+// };
+
+// app.get("/urls", (req, res) => {
+//   let templateVars = { urls: urlDatabase };
+//   res.render("urls_index", templateVars);
+// });
