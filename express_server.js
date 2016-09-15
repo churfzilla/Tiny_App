5d8407f8b059a067cb694a790c16d21b9c03798e
Chad Bruhaug <churfzilla@gmail.com>
2016-09-14 17:33:40 -0700

    Set up string generator

--- express_server.js
+++ express_server.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 app.set("view engine", "ejs");
-var PORT = process.env.PORT || 8080; // default port 8080
-
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.listen(PORT, () => {
@@ -13,14 +14,19 @@ app.get('/urls', (req, res) => {
         { shortUrl: "b2xVn2", fullUrl: "http://www.lighthouselabs.ca" },
         { shortUrl: "9sm5xK", fullUrl: "http://www.google.com" }
     ];
-    var tagline = "Displaying Tiny App Links";
-
     res.render('urls_index', {
         urlDatabase: urlDatabase,
-        tagline: tagline
     });
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 // app.get("/urls/:id", (req, res) => {
 //   let templateVars = { shortURL: req.params.id };
 //   res.render("urls_show", templateVars);
