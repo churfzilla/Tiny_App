6ba09fb4b1b3e64f8bbbb8161628ab2ee71dbf78
Chad Bruhaug <churfzilla@gmail.com>
2016-09-15 15:02:33 -0700

    Correctly sends client to shortened URL

--- express_server.js
+++ express_server.js
@@ -19,34 +19,49 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//INDEX PAGE
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-   let longURL = req.body.longURL;
-  res.redirect(longURL);
-});
-
-//NEW URL PAGE
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+ res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
-   urlDatabase[generateRandomString()] = req.body.longURL;
-   console.log({urls: urlDatabase});
-   res.redirect('/urls');
- });
+ urlDatabase[generateRandomString()] = req.body.longURL;
+ res.redirect('/urls');
+});
 
- app.get('/urls/:id', (req, res) => {
-   let templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-   res.render('urls_show', templateVars);
- });
+app.get('/urls', (req, res) => {
+ let templateVars = { urls: urlDatabase };
+ res.render('urls_index', templateVars);
+});
 
-//Tells console what port it is listening on
+app.get('/urls/:id', (req, res) => {
+ let templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+ res.render('urls_show', templateVars);
+});
+
+app.put('/urls/:id', (req, res) => {
+ urlDatabase[req.params.id] = req.body.url;
+ res.redirect('/urls');
+});
+
+app.delete('/urls/:id', (req, res) => {
+ delete urlDatabase[req.params.id];
+ res.redirect('/urls');
+});
+
+app.get("/u/:shortURL", (req, res) => {
+ let longURL = urlDatabase[req.params.shortURL];
+ if (longURL === undefined) {
+   var templateVars = { shortURL: req.params.shortURL };
+   res.status(404).render('not_found', templateVars);
+ } else {
+   res.status(301).redirect(longURL);
+ }
+});
+
+app.get('/urls.json', (req, res) => {
+ res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
-  console.log(`Tiny app listening on port ${PORT}!`);
+ console.log(`Example app listening on port ${PORT}!`);
 });
