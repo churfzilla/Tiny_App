8373eec30ad0e4cfb5366a97b61d0ab0637512c1
Chad Bruhaug <churfzilla@gmail.com>
2016-09-17 02:40:42 -0700

    Cleaned things up

--- function_index.js
+++ function_index.js
@@ -1,38 +1,8 @@
 'use strict';
-
 //Generates a random 6 character string ----------------------------------------------
-exports.generateRandomString = function(){
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for( var i=0; i < 6; i++ )
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  return text;
-};
-//GETs The long & short URL from MongoDb------------------------------------------------
-exports.getURLs = function(db, cb) {
-  db.collection('urls').find().toArray((err, result) => {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, result);
-  });
+function generateRandomString() {
+  return Math.random().toString(36).substr(2,6);
 }
-//Gets the long URL from the short URL's MongoDb document----------------------------------
-exports.getLongURL = function(db, shortURL, cb) {
-  let query = { 'shortURL': shortURL };
-  db.collection('urls').findOne(query, (err, result) => {
-    if (err) {
-      return cb(err);
-    }
-    if (result === null) {
-      return cb('not_found', undefined);
-    }
-    else {
-      return cb(null, result.longURL);
-    }
-  });
-}
-//Insert New URL to MongoDb----------------------------------------------------------
 exports.insertURL = function(db, longURL, cb) {
   let newURL = {
     shortURL: generateRandomString(),
@@ -49,7 +19,8 @@ exports.insertURL = function(db, longURL, cb) {
 exports.updateURL = function(db, shortURL, longURL, cb) {
   db.collection('urls').updateOne(
     { 'shortURL': shortURL },
-    { $set: { 'longURL': longURL }
+    {
+      $set: { 'longURL': longURL }
     }, (err, result) => {
       if (err) {
         return cb(err);
@@ -66,4 +37,27 @@ exports.deleteURL = function(db, shortURL, cb) {
     return cb(null, result);
   });
 }
-
+//Gets the long URL from the short URL's MongoDb document----------------------------------
+exports.getLongURL = function(db, shortURL, cb) {
+  let query = { 'shortURL': shortURL };
+  db.collection('urls').findOne(query, (err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    if (result === null) {
+      return cb('not_found', undefined);
+    } else {
+      return cb(null, result.longURL);
+    }
+  });
+}
+//GETs The long & short URL from MongoDb------------------------------------------------
+exports.getURLs = function(db, cb) {
+  db.collection('urls').find().toArray((err, result) => {
+    if (err) {
+      return cb(err);
+    }
+    return cb(null, result);
+  });
+}
+debugger;
